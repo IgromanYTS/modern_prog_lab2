c3a6941d5df490b27b1c803dffe9a7076d71587f
IgromanYTS <83410566+IgromanYTS@users.noreply.github.com>
2024-04-19 10:56:12 +0300

    Add files via upload

---
+++ lab2.Tests/StrategyMulTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace lab2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [DataTestMethod]
+        [DataRow(2, 2, 4)]
+        [DataRow(2, -3, -6)]
+        [DataRow(-2, 4, -8)]
+        [DataRow(-12, -2, 24)]
+        [DataRow(1, int.MaxValue, int.MaxValue)]
+        [DataRow(1, int.MinValue, int.MinValue)]
+        public void Test3(int a, int b, int expected) 
+        {
+            Context context = new Context();
+            context.setStrategy(new StrategyMul());
+            int result = context.executeStrategy(a, b);
+            Assert.AreEqual(expected, result);
+        }
+        
+    }
+}
---
+++ lab2/StrategyMul.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class StrategyMul : IStrategy
+    {
+        public int execute(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
